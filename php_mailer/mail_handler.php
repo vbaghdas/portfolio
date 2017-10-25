8ffb16109beb9381b27a508a11ce18d55ade79d1
vbaghdas <vbaghdas@gmail.com>
2017-10-25 13:18:18 -0700

    made changes to php_mail handler

--- php_mailer/mail_handler.php
+++ php_mailer/mail_handler.php
@@ -1,41 +1,55 @@
 <?php
+
 require_once('email_config.php');
 require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+
+// Create an instance of php mailer
 $mail = new PHPMailer;
-$mail->SMTPDebug = 3;           // Enable verbose debug output. Change to 0 to disable debugging output.
 
-$mail->isSMTP();                // Set mailer to use SMTP.
-$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
-$mail->SMTPAuth = true;         // Enable SMTP authentication
+// Set a host
+$mail->Host = 'smtp.gmail.com';
 
-//Form
+// Set mailer to use SMTP.
+$mail->isSMTP();
+
+// Enable SMTP authentication
+$mail->SMTPAuth = true;         
+
+// Contact Form
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$mail->Username = EMAIL_USER;   // SMTP username
-$mail->Password = EMAIL_PASS;   // SMTP password
-$mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
-$mail->Port = 587;              // TCP port to connect to
-$options = array(
-    'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-    )
-);
-$mail->smtpConnect($options);
-$mail->From = $visitor_email;  // sender's email address (shows in "From" field)
-$mail->FromName = $name;   // sender's name (shows in "From" field)
-$mail->addAddress('vbaghdas@gmail.com', 'First Recipient');  // Add a recipient
-$mail->addReplyTo($visitor_email);                          // Add a reply-to address
+// Set login details for gmail account
+$mail->Username = EMAIL_USER;
+$mail->Password = EMAIL_PASS;
 
-$mail->isHTML(true);                                  // Set email format to HTML
+// Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
+$mail->SMTPSecure = 'ssl';    
+
+// TCP port to connect to
+$mail->Port = 465;
+
+$mail->smtpConnect($options);
+// Sender's email address (shows in "From" field)
+$mail->From = $visitor_email;
+
+// Sender's name (shows in "From" field)
+$mail->FromName = $name;
+
+// Add a recipient
+$mail->addAddress('vbaghdas@gmail.com', 'First Recipient');
+
+// Add a reply-to address
+$mail->addReplyTo($visitor_email);                          
+
+// Set email format to HTML
+$mail->isHTML(true);
 
 $mail->Subject = 'Re: Portfolio Contact Form';
 $mail->Body    = $message;
-$mail->AltBody = $message;
 
+// Send an email
 if(!$mail->send()) {
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $mail->ErrorInfo;
